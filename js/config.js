3ea4a0a66075c03cedf9fe4f526c4a4426779fe8
camileharumi <camileharumi@gmail.com>
2017-09-25 02:36:45 -0300

    Projeto ReForçar

---
+++ js/config.js
@@ -0,0 +1,29 @@
+var app = angular.module('reForcar', ['ngRoute']);
+
+app.config(function ($routeProvider){
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'templates/home.html',
+			controller: 'homeController'
+		})
+		.when('/sobre', {
+			templateUrl: 'templates/sobre.html',
+			controller: 'sobreController'
+		})
+		.when('/cadastroInstituicao', {
+			templateUrl: 'templates/cadastroInstituicao.html',
+			controller: 'cadastroInstituicaoController'
+		})
+		.when('/cadastroVoluntario', {
+			templateUrl: 'templates/cadastroVoluntario.html',
+			controller: 'cadastroVoluntarioController'
+		})
+		.when('/login', {
+			templateUrl: 'templates/login.html',
+			controller: 'loginController'
+		})
+		.otherwise({
+			redirectTo:'/home'
+		});
+});
+
